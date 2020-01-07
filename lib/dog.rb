417aeca2da65e43269513ec5f0077236d3e5d2b9
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-07 04:34:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,16 @@ class Dog
     puts "woof!"
   end
   
-  
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
 end
+
+  def name
+    @this_dogs_name
+  end
 
 fido = Dog.new
 
-# fido.name = "Fido"
-
+fido.name = "Fido"
+puts fido.name
 fido.bark
